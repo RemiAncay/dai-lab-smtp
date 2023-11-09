9df08257676dedcd0cda01aae62fde49fad445d8
Raynobrak <lcs.charbonnier@gmail.com>
2023-11-09 12:01:41 +0100

    implement EmailList and MessagesList

--- code/dai-lab-smtp/untitled/src/EmailList.java
+++ code/dai-lab-smtp/untitled/src/EmailList.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class EmailList {
+    ArrayList<String> addresses;
+    String path;
+
     public EmailList(String emailListPath) {
-        // todo
+        addresses = new ArrayList<>();
+        path = emailListPath;
+        loadAddresses();
+    }
+
+    private void loadAddresses() {
+        try(var reader = new FileReader(path);
+            var bufferedReader = new BufferedReader(reader))
+        {
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                addresses.add(line);
+            }
+        }
+        catch(IOException e) {
+            return;
+        }
+    }
+
+    public void chooseRandomAddress() {
+        //todo
+    }
+
+    public void chooseRandomAddresses() {
+        //todo
     }
 }
--- code/dai-lab-smtp/untitled/src/MessagesList.java
+++ code/dai-lab-smtp/untitled/src/MessagesList.java
@@ -1,5 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MessagesList {
+    private static final String MESSAGES_SEPARATOR = ";";
+    private static final String DEFAULT_MESSAGE = "Default message :(";
+
+    private final String path;
+    private ArrayList<String> messages;
     public MessagesList(String messagesPath) {
-        // todo
+        messages = new ArrayList<>();
+        this.path = messagesPath;
+        loadMessageList();
+    }
+
+    private void loadMessageList() {
+        String data;
+        try {
+            data = Files.readString(Path.of(path));
+        }
+        catch(IOException e) {
+            return;
+        }
+
+        var tokens = data.split(MESSAGES_SEPARATOR);
+        for(var tok : tokens)
+            if(!tok.isEmpty())
+                messages.add(tok);
+    }
+
+    public String chooseRandomMessage() {
+        if(messages.isEmpty())
+            return DEFAULT_MESSAGE;
+
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(messages.size());
+        return messages.get(randomIndex);
     }
 }
